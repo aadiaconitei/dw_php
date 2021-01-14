c7612236ca0cfd9d907199e88266a2de81e476fe
Adrian Adiaconitei <adrian@hipo.ro>
2021-01-14 13:18:59 +0200

    curs 1

---
+++ despre.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Pagina despre companie</title>
+	<?php include("html/head.html");?>
+</head>
+
+<body>
+	<h1>Pagina Despre noi</h1>
+	<?php include("html/header.html");?>
+	<?php include("html/despre.html");?>
+	<?php include("html/footer.html");?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Proiect nou 2021</title>
+	<?php include("html/head.html");?>
+</head>
+
+<body>
+	<h1>Proiect nou 2021</h1>
+	<?php include("html/header.html");?>
+	<?php include("html/acasa.html");?>
+	<?php include("html/footer.html");?>
+</body>
+</html>
