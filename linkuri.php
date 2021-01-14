6b896972678cb30cfa38e46d3b66fccc39b92b36
Radu Gasler <miezuit@gmail.com>
2021-01-14 20:44:07 +0200

    curs 2 clasa

--- linkuri.php
+++ linkuri.php
@@ -2,15 +2,14 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Pagina despre companie</title>
+<title>Proiect nou 2021</title>
 	<?php include("html/head.html");?>
 </head>
 
 <body>
-	<h1>Pagina Despre noi</h1>
+	<h1>Pagina de linkuri</h1>
 	<?php include("html/header.html");?>
-	<?php include("html/despre.html");?>
+	<?php include("html/linkuri.html");?>
 	<?php include("html/footer.html");?>
-	
 </body>
 </html>
--- text.php
+++ text.php
@@ -2,15 +2,14 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Pagina despre companie</title>
+<title>Proiect nou 2021</title>
 	<?php include("html/head.html");?>
 </head>
 
 <body>
-	<h1>Pagina Despre noi</h1>
+	<h1>Elemente HTML de text</h1>
 	<?php include("html/header.html");?>
-	<?php include("html/despre.html");?>
+	<?php include("html/text.html");?>
 	<?php include("html/footer.html");?>
-	
 </body>
 </html>
