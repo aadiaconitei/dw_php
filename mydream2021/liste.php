34500f3268b26a5d92eba8c786771a466a6785fa
Adrian Adiaconitei <adrian@hipo.ro>
2021-02-18 15:48:17 +0200

    proiect

---
+++ mydream2021/liste.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>My Dream - Adobe Dreamweaver CC - Adrian Adiaconitei </title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link href="assets/css/style.css" rel="stylesheet" type="text/css">
+	</head>
+<body>
+	<!-- Main Container -->
+	<div class="container"> 
+  		<!-- Header -->
+  		<?php include('html/header.html');?>
+		<!--End header-->
+		<!-- Banner Section -->
+		<?php //include('html/banner.html');?>
+		<!-- End Banner Section -->
+  		<!-- Acasa Section -->
+  		<?php include('html/liste.html');?>
+  		<!-- Footer -->
+  		<?php include('html/footer.html');?>
+	</div>
+	<!-- Main Container Ends -->	
+</body>
+</html>
